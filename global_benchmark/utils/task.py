203c21ea89b2d058710aeeffc794ecf50b56e455
Blyzi <flash.spaces@gmail.com>
2025-07-15 14:18:57 +0200

    feat: added arc/hellaswag/ifeval/xnli/belebele benchmarks

--- global_benchmark/utils/task.py
+++ global_benchmark/utils/task.py
@@ -16,6 +16,19 @@ class Task(str, Enum):
 
     MMLU = "mmlu"
 
+    ARC_EASY = "arc_easy"
+    ARC_EASY_CHAT = "arc_easy_chat"
+    ARC_CHALLENGE = "arc_challenge"
+    ARC_CHALLENGE_CHAT = "arc_challenge_chat"
+
+    HELLASWAG = "hellaswag"
+
+    IFEVAL = "ifeval"
+
+    XNLI = "xnli"
+
+    BELEBELE = "belebele"
+
     # BigCodeBenchmark
     BIG_CODE_BENCHMARK_COMPLETE = "big_code_bench_complete"
     BIG_CODE_BENCHMARK_INSTRUCT = "big_code_bench_instruct"
